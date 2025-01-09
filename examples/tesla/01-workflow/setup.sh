b75042a46991b9932650dcda83b063cf22d29080
weihong.xu <xuweihong.cn@gmail.com>
2025-01-09 17:36:00 +0800

    refactor tesla example

--- examples/tesla/01-workflow/setup.sh
+++ examples/tesla/01-workflow/setup.sh
@@ -3,5 +3,6 @@
 # this script is to generate deepmd train data and lammps structure files from aimd.xyz
 set -e
 
-ai2-kit tool ase read ./00-init/aimd.xyz - to_dpdata - write ./00-init/dp-data
-ai2-kit tool ase read ./00-init/aimd.xyz --index '::20' - write ./00-init/lammps-data/
+ai2-kit tool ase read ./00-init/aimd.xyz - to_dpdata - write ./10-workdir/dp-init-data
+
+ai2-kit tool ase read ./00-init/aimd.xyz --index '::20' - write_frames ./10-workdir/lammps-data/{i:03d}.data --format lammps-data --specorder "[Ag,O]"
