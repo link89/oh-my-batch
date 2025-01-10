6e4989d351b60e1fe63d61f4fc00420067926f71
weihong.xu <xuweihong.cn@gmail.com>
2025-01-10 09:29:04 +0800

    refactor tesla example

--- examples/tesla/run.sh
+++ examples/tesla/run.sh
@@ -6,6 +6,6 @@ pip install "ai2-kit>=0.22.6" "oh-my-batch>=0.4.7"
 
 ./01-workflow/setup.sh
 
-ITER_NAME="000" ./01-workflow/iter.sh
-ITER_NAME="001" ./01-workflow/iter.sh
-ITER_NAME="002" ./01-workflow/iter.sh
+ITER_NAME="000" ./01-workflow/iter-basic-lammps-cp2k.sh
+ITER_NAME="001" ./01-workflow/iter-basic-lammps-cp2k.sh
+ITER_NAME="002" ./01-workflow/iter-basic-lammps-cp2k.sh
