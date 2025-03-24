7d55ee18e24a837ddc37b35ae0c92221e1cca911
weihong.xu <xuweihong.cn@gmail.com>
2025-03-24 17:59:36 +0800

    add more params for benchmark

--- examples/lammps-benchmark/run.sh
+++ examples/lammps-benchmark/run.sh
@@ -9,6 +9,7 @@ cat > ./out/lmp.sh <<EOF
 set -e
 
 export OMP_NUM_THREADS=@OMP_NUMS
+export OPENBLAS_NUM_THREADS=@OPENBLAS_NUMS
 
 [ -f lmp.done ] || {
 mpirun -np @MPI_NUMS lmp -in @LMP_IN -var data_file @LMP_DATA
@@ -37,10 +38,11 @@ EOF
 omb combo \
     add_files LMP_IN ./config/in.lmp --abs -\
     add_files LMP_DATA ./config/data.lmp --abs -\
-    add_var OMP_NUMS 64 32 16  8  4  2  1 -\
+    add_var OMP_NUMS  64 32 16  8  4  2  1 -\
     add_var MPI_NUMS  1  2  4  8 16 32 64 -\
+    add_var OPENBLAS_NUMS 1 8 16 -\
     set_broadcast MPI_NUMS -\
-    make_files ./out/job-omp-{OMP_NUMS}-mpi-{MPI_NUMS}/run.sh --template ./out/lmp.sh --mode 755 -\
+    make_files ./out/job-omp-{OMP_NUMS}-mpi-{MPI_NUMS}-blas-{OPENBLAS_NUMS}/run.sh --template ./out/lmp.sh --mode 755 -\
     done
 
 omb batch \
