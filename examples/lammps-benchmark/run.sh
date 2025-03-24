b148a307b7af36713f72bda306afb9fd84bafee4
weihong.xu <xuweihong.cn@gmail.com>
2025-03-24 17:51:49 +0800

    add checkpoint for lammps example

--- examples/lammps-benchmark/run.sh
+++ examples/lammps-benchmark/run.sh
@@ -9,7 +9,11 @@ cat > ./out/lmp.sh <<EOF
 set -e
 
 export OMP_NUM_THREADS=@OMP_NUMS
+
+[ -f lmp.done ] || {
 mpirun -np @MPI_NUMS lmp -in @LMP_IN -var data_file @LMP_DATA
+touch lmp.done
+}
 
 EOF
 
@@ -33,8 +37,8 @@ EOF
 omb combo \
     add_files LMP_IN ./config/in.lmp --abs -\
     add_files LMP_DATA ./config/data.lmp --abs -\
-    add_var OMP_NUMS 64 32 16 8  4  -\
-    add_var MPI_NUMS 1  2  4  8 16  -\
+    add_var OMP_NUMS 64 32 16  8  4  2  1 -\
+    add_var MPI_NUMS  1  2  4  8 16 32 64 -\
     set_broadcast MPI_NUMS -\
     make_files ./out/job-omp-{OMP_NUMS}-mpi-{MPI_NUMS}/run.sh --template ./out/lmp.sh --mode 755 -\
     done
@@ -42,5 +46,5 @@ omb combo \
 omb batch \
     add_work_dirs ./out/job-*/ --abs -\
     add_header_files ./out/slurm.sh -\
-    add_cmds ./run.sh -\
+    add_cmds "./run.sh || true" -\
     make ./out/batch-{i}.slurm --concurrency 1
